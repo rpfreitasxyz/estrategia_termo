83a18467b4f436aea70f304567aaf8114d9607cb
rpfreitasxyz <rpfreitas@poli.ufrj.br>
2022-02-08 00:44:12 -0300

    continuacao do script, talvez base R com vetorizacao resolva melhor

--- script.R
+++ script.R
@@ -8,3 +8,13 @@ palavras_brutas <- read_table("pt-br-master/dicio") %>%
 
 palavras <- palavras_brutas %>%
   separate(sem_acentos, into = c(NA, "letra1", "letra2", "letra3", "letra4", "letra5"), sep = "", remove = FALSE)
+
+palavra_resposta <- slice(palavras, round(runif(1, min = 1, max = nrow(palavras)))) %>% select(sem_acentos) %>% unlist() %>% unname()
+
+palavra_chute <- slice(palavras, round(runif(1, min = 1, max = nrow(palavras)))) %>% select(sem_acentos) %>% unlist() %>% unname()
+
+resp <- c(str_split(palavra_resposta, "")[[1]], seq(1, 5))
+chute <- c(str_split(palavra_chute, "")[[1]], seq(1, 5))
+
+# AMARELA
+chute[4] %in% resp[1:5]
